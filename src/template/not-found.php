c9b9c311f0b7bc07fcb570008c8a20c2876b6722
opportus <opportus@gmail.com>
2017-10-19 22:52:43 +0200

    Adds bootstrap script on all pages

--- src/template/not-found.php
+++ src/template/not-found.php
@@ -16,9 +16,8 @@
 <!-- JS Scripts
 ================================================== -->
 <!-- Placed at the end of the document so the pages load faster -->
-<!-- HTML5 shim and Respond.js IE8 support of HTML5 elements and media queries -->
-<!--[if lt IE 9]>
-	<script src="https://oss.maxcdn.com/libs/html5shiv/3.7.0/html5shiv.js"></script>
-	<script src="https://oss.maxcdn.com/libs/respond.js/1.3.0/respond.min.js"></script>
-<![endif]-->
+<!-- jQuery -->
+<script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
+<!-- Bootstrap Core JavaScript -->
+<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
 <?php require_once('footer.php'); ?>
--- src/template/post-edit.php
+++ src/template/post-edit.php
@@ -49,11 +49,8 @@
 <!-- Placed at the end of the document so the pages load faster -->
 <!-- jQuery -->
 <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
-<!-- HTML5 shim and Respond.js IE8 support of HTML5 elements and media queries -->
-<!--[if lt IE 9]>
-	<script src="https://oss.maxcdn.com/libs/html5shiv/3.7.0/html5shiv.js"></script>
-	<script src="https://oss.maxcdn.com/libs/respond.js/1.3.0/respond.min.js"></script>
-<![endif]-->
+<!-- Bootstrap Core JavaScript -->
+<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
 <!-- Form AJAX -->
 <script src="<?php echo APP_URL . '/js/form-ajax.min.js'; ?>"></script>
 <?php require_once('footer.php'); ?>
